08ff5e702c18ef1271673f7b5d798bccfcc92693
Keanu Naicker <keanu.naicker27@gmail.com>
2026-01-04 21:20:48 +0100

    Create providers.tf

---
+++ infrastructure/terraform/providers.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_providers {
+    azurerm = { source = "hashicorp/azurerm", version = "~> 3.0" }
+    aws     = { source = "hashicorp/aws", version = "~> 5.0" }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+provider "aws" {
+  region = "eu-central-1" # Frankfurt
+}
